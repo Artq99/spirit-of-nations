6fb9a54587b2ad8a8f24c168777844ef46a97c82
Artq99 <amatracki@gmail.com>
2022-10-30 15:24:54 +0100

    Refactor grid module.

--- son/grid.py
+++ son/grid.py
@@ -3,50 +3,69 @@ import pygame
 from son.resources import ResourceManager
 
 
+GRID_CELL_SIZE = 50
+GRID_CELL_SIZE_XY = (GRID_CELL_SIZE, GRID_CELL_SIZE)
+COLOR_FOCUS = (100, 100, 0)
+
+
 class GridCell:
 
-    def __init__(self, pos, resource_manager: ResourceManager):
-        self.pos = pos
-        x = pos[0] * 50
-        y = pos[1] * 50
-        self.resource_manager = resource_manager
-        self.rect = pygame.Rect(x, y, 50, 50)
-        self.surface = self.resource_manager.get_resource("grass")
+    @staticmethod
+    def _calc_pixel_pos(grid_pos):
+        grid_pos_x, grid_pos_y = grid_pos
+        return grid_pos_x * GRID_CELL_SIZE, grid_pos_y * GRID_CELL_SIZE
+
+    def __init__(self, grid_pos, resource_manager: ResourceManager):
+        self.grid_pos = grid_pos
+        self.pixel_pos = GridCell._calc_pixel_pos(grid_pos)
+
+        self.surface = resource_manager.get_resource("grass")
+        self.rect = pygame.Rect(self.pixel_pos, GRID_CELL_SIZE_XY)
 
         self.focused = False
 
     def draw(self, destination_surface: pygame.Surface, delta):
-        rect = pygame.Rect(self.rect.left - delta[0], self.rect.top - delta[1], 50, 50)
-        destination_surface.blit(self.surface, rect)
+        rect_delta = self._get_rect_with_delta(delta)
+        destination_surface.blit(self.surface, rect_delta)
 
         if self.focused:
-            pygame.draw.rect(destination_surface, (100, 100, 0), rect, width=1)
-            print(self.pos)
+            pygame.draw.rect(destination_surface, COLOR_FOCUS, rect_delta, width=1)
 
     def update_focus(self, mouse_pos, delta):
-        rect = pygame.Rect(self.rect.left - delta[0], self.rect.top - delta[1], 50, 50)
-        self.focused = rect.collidepoint(mouse_pos)
+        rect_delta = self._get_rect_with_delta(delta)
+        self.focused = rect_delta.collidepoint(mouse_pos)
+
+    def _get_rect_with_delta(self, delta):
+        delta_x, delta_y = delta
+        delta_pos = (self.rect.left - delta_x, self.rect.top - delta_y)
+        return pygame.Rect(delta_pos, GRID_CELL_SIZE_XY)
 
 
 class Grid:
 
-    def __init__(self, size, resource_manager):
-        self.array = []
+    @staticmethod
+    def _create_array(size, resource_manger):
+        size_x, size_y = size
+        array = []
 
-        for y in range(size[1]):
+        for y in range(size_y):
             row = []
-            for x in range(size[0]):
-                cell = GridCell((x, y), resource_manager)
+            for x in range(size_x):
+                cell = GridCell((x, y), resource_manger)
                 row.append(cell)
+            array.append(row)
 
-            self.array.append(row)
+        return array
+
+    def __init__(self, size, resource_manager):
+        self._array = Grid._create_array(size, resource_manager)
 
     def draw(self, destination_surface: pygame.Surface, delta):
-        for row in self.array:
+        for row in self._array:
             for cell in row:
                 cell.draw(destination_surface, delta)
 
     def update_focus(self, mouse_pos, delta):
-        for row in self.array:
+        for row in self._array:
             for cell in row:
                 cell.update_focus(mouse_pos, delta)
