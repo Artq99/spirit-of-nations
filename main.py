93532389ee3f27b249260de66316ec81868a6c74
Artq99 <amatracki@gmail.com>
2023-02-06 13:43:21 +0100

    Add an option to give the wanted resolution as an argument.

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import re
+import sys
+
 from son import SpiritOfNationsApp
 
+
+def get_resolution() -> tuple[int, int]:
+    """
+    Check if the resolution has been given as an argument.
+    When it has, parse it, when not, get the default value.
+    :return: resolution
+    """
+
+    # Default values
+    width = 1920
+    height = 1080
+
+    for arg in sys.argv:
+        match = re.search('^r=(\\d+)x(\\d+)$', arg)
+        if match is not None:
+            width = int(match.group(1))
+            height = int(match.group(2))
+
+    return width, height
+
+
 if __name__ == "__main__":
-    app = SpiritOfNationsApp((1920, 1080))
+    app = SpiritOfNationsApp(get_resolution())
     app.run()
