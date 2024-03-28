af86e140c93485f4f02bd9b75ab37e5a3c6756ed
Artq99 <amatracki@gmail.com>
2024-03-28 23:49:07 +0100

    Move the class MapCell into a separate file.

---
+++ son/gameplay/map/_map_cell.py
@@ -0,0 +1,135 @@
+from typing import List
+
+import pygame
+from pygame import Rect, MOUSEMOTION, MOUSEBUTTONUP, Surface
+from pygame.event import Event
+
+from son.core.base import Lifecycle
+from son.core.events import EDGE_SCROLL, SELECT_MAP_OBJECT, SHOW_MAP_OBJECT_INFO, HIDE_MAP_OBJECT_INFO, SHOW_CELL_INFO
+from son.core.resources import ResourceManager
+from son.core.utils.decorators import override
+from son.core.vectors import VectorInt2D
+from son.gameplay._types import CellInfo
+from son.gameplay.map._constants import GRID_CELL_SIZE, GRID_CELL_SIZE_XY, COLOR_FOCUS
+from son.gameplay.map._map_objects_base import MapObject
+
+
+class MapCell(Lifecycle):
+    """
+    Single cell of a grid-based map.
+    """
+
+    @staticmethod
+    def _calc_pixel_pos(grid_pos: VectorInt2D) -> VectorInt2D:
+        grid_pos_x, grid_pos_y = grid_pos
+        return grid_pos_x * GRID_CELL_SIZE, grid_pos_y * GRID_CELL_SIZE
+
+    def __init__(self, grid_pos: VectorInt2D, resource_manager: ResourceManager) -> None:
+        self._grid_pos = grid_pos
+
+        self._rect = Rect(MapCell._calc_pixel_pos(grid_pos), GRID_CELL_SIZE_XY)
+        self._rect_delta = self._get_rect_with_delta((0, 0))
+
+        self._is_focused = False
+
+        # TODO terrain type and surface are hardcoded for now
+        self._terrain_type = "grass"
+        self._surface = resource_manager.get_resource(self._terrain_type)
+
+        self._map_objects: List[MapObject] = list()
+
+    @property
+    def rect(self) -> Rect:
+        """
+        Rect of this cell.
+        """
+        return self._rect.copy()
+
+    @property
+    def is_focused(self) -> bool:
+        """
+        Is the cell focused?
+        """
+        return self._is_focused
+
+    @property
+    def info(self) -> CellInfo:
+        """
+        Information about this cell.
+        """
+        return CellInfo(
+            grid_pos=self._grid_pos,
+            terrain_type=self._terrain_type,
+            objects=[o.info for o in self._map_objects]
+        )
+
+    @override
+    def pre_update(self, *args, **kwargs) -> None:
+        for map_object in self._map_objects:
+            map_object.pre_update(*args, **kwargs)
+
+    @override
+    def update(self, *args, **kwargs) -> None:
+        for map_object in self._map_objects:
+            map_object.update(self, *args, **kwargs)
+
+    @override
+    def handle_event(self, event: Event, *args, **kwargs) -> bool:
+        for map_object in self._map_objects:
+            if map_object.handle_event(event, *args, **kwargs):
+                return True
+
+        if event.type == EDGE_SCROLL:
+            self._rect_delta = self._get_rect_with_delta(event.delta)
+            self._is_focused = self._rect_delta.collidepoint(event.pos)
+            return False
+
+        elif event.type == MOUSEMOTION:
+            self._is_focused = self._rect_delta.collidepoint(event.pos)
+            return False
+
+        if self._is_focused:
+            if event.type == MOUSEBUTTONUP:
+
+                # Left mouse click - selecting an object on the map
+                if event.button == 1:
+                    # If there is an object in the cell:
+                    if len(self.info.objects) > 0:
+                        # - we notify the map about the new selected object
+                        pygame.event.post(Event(SELECT_MAP_OBJECT, {"map_object": self._map_objects[0]}))
+                        # - we notify the UI that the info window should be shown
+                        pygame.event.post(Event(SHOW_MAP_OBJECT_INFO, {"map_object_info": self.info.objects[0]}))
+                    # If there are no objects in the cell, i.e. it is empty, we must deselect an object:
+                    else:
+                        # - we notify the map and tell it that the selected object is None
+                        pygame.event.post(Event(SELECT_MAP_OBJECT, {"map_object": None}))
+                        # - we notify the UI that the info panel should be hidden
+                        pygame.event.post(Event(HIDE_MAP_OBJECT_INFO))
+                # Middle mouse click - viewing the cell info
+                # It should be the right mouse click (3), but it cannot be detected for some reason.
+                if event.button == 2:
+                    pygame.event.post(Event(SHOW_CELL_INFO, {"cell_info": self.info, "pos": self._rect_delta.center}))
+                    return True
+
+        return False
+
+    def _get_rect_with_delta(self, delta: VectorInt2D) -> Rect:
+        delta_x, delta_y = delta
+
+        rect_delta = self._rect.copy()
+        rect_delta.left -= delta_x
+        rect_delta.bottom -= delta_y
+
+        return rect_delta
+
+    @override
+    def draw(self, destination_surface: Surface, *args, **kwargs) -> None:
+        destination_surface.blit(self._surface, self._rect_delta)
+        if self._is_focused:
+            pygame.draw.rect(destination_surface, COLOR_FOCUS, self._rect_delta, width=1)
+
+        for map_object in self._map_objects:
+            map_object.draw(destination_surface, *args, **kwargs, cell_rect=self._rect_delta)
+
+    def add_object(self, map_object: MapObject):
+        self._map_objects.append(map_object)
