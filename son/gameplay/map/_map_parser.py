3cfcc733334b8932acfb9ef0791d43b1821a4ef8
Artq99 <amatracki@gmail.com>
2025-04-15 22:29:31 +0200

    Simplify parsing map objects in the map parser.

--- son/gameplay/map/_map_parser.py
+++ son/gameplay/map/_map_parser.py
@@ -1,11 +1,25 @@
 import os.path
-from typing import List
+from typing import List, Dict, Type
 from xml.etree import ElementTree
 
 from son.core.resources import ResourceManager
 from son.gameplay.map._map_cell import MapCell
 from son.gameplay.map.objects.static import Forest, Boulders
 from son.gameplay.map.objects.units import Tribe
+
+# Tags for map objects and their corresponding types
+_OBJECTS: Dict[str, Type] = {
+    "tribe": Tribe,
+    "forest": Forest,
+    "boulders": Boulders
+}
+
+
+class MapParseException(Exception):
+    """
+    Exception raised when a map file could not be parsed.
+    """
+    pass
 
 
 def parse_map(name: str, resource_manager: ResourceManager) -> List[List[MapCell]]:
@@ -14,6 +28,7 @@ def parse_map(name: str, resource_manager: ResourceManager) -> List[List[MapCell
 
     :param name: name of the map
     :param resource_manager: resource manager
+    :raises MapParseException: when the given file could not be parsed
     """
     path = os.path.join('maps', name + '.map')
     tree = ElementTree.parse(path)
@@ -36,15 +51,12 @@ def parse_map(name: str, resource_manager: ResourceManager) -> List[List[MapCell
             map_objects = cell_info.find("./objects")
             if map_objects is not None:
                 for map_object in map_objects:
-                    if map_object.tag == "tribe":
-                        tribe = Tribe(resource_manager)
-                        cell.add_object(tribe)
-                    if map_object.tag == "forest":
-                        forest = Forest(resource_manager)
-                        cell.add_object(forest)
-                    if map_object.tag == "boulders":
-                        boulders = Boulders(resource_manager)
-                        cell.add_object(boulders)
+                    try:
+                        object_type = _OBJECTS[map_object.tag]
+                        instance = object_type(resource_manager)
+                        cell.add_object(instance)
+                    except KeyError:
+                        raise MapParseException("Unknown object: {}".format(map_object.tag))
 
             row.append(cell)
         array.append(row)
