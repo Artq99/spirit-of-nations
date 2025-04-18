1b775cf637342e1eb0f82a05b5cf4022caff23fc
Artq99 <amatracki@gmail.com>
2025-04-19 00:51:50 +0200

    Make forest grow speed related to the current month.

--- son/gameplay/map/objects/static/_forest.py
+++ son/gameplay/map/objects/static/_forest.py
@@ -1,3 +1,4 @@
+import random
 from typing import List
 
 from pygame import Surface
@@ -6,6 +7,49 @@ from pygame.event import Event
 from son.core.events import START_TURN
 from son.core.resources import ResourceManager
 from son.gameplay.map.objects import MapObject, ModifiersHolder
+from son.gameplay.types import TurnInfo
+
+
+class ForestStats:
+    """
+    Stats of the forest map object.
+    """
+
+    def __init__(self) -> None:
+        self._density = 0
+
+    @property
+    def density(self) -> int:
+        """
+        Density of the forest in percents (0% - 100%).
+        """
+        return self._density
+
+    def grow(self, turn_info: TurnInfo) -> None:
+        """
+        Make the forest grow.
+
+        It depends on the current month how much the forest is going to grow.
+
+        :param turn_info: Information about the current turn
+        """
+        if self._density == 100:
+            return
+
+        month: str = turn_info.month
+        grow_value: int = 0
+        if month == "April":
+            grow_value = random.randint(0, 1)
+        elif month == "May":
+            grow_value = random.randint(0, 2)
+        elif month == "June":
+            grow_value = random.randint(0, 2)
+        elif month == "July":
+            grow_value = random.randint(0, 1)
+
+        self._density += grow_value
+        if self._density > 100:
+            self._density = 100
 
 
 class Forest(MapObject, ModifiersHolder):
@@ -16,7 +60,7 @@ class Forest(MapObject, ModifiersHolder):
 
         self._modifiers.append(("movement_cost", 2))
 
-        self._density = 0
+        self._stats = ForestStats()
 
         self._surfaces: List[Surface] = [
             resource_manager.get_resource("object.forest_01.stage_1"),
@@ -26,16 +70,16 @@ class Forest(MapObject, ModifiersHolder):
         ]
 
     def _get_surface(self) -> Surface:
-        if self._density < 25:
+        if self._stats.density < 25:
             return self._surfaces[0]
-        elif self._density < 50:
+        elif self._stats.density < 50:
             return self._surfaces[1]
-        elif self._density < 75:
+        elif self._stats.density < 75:
             return self._surfaces[2]
         else:
             return self._surfaces[3]
 
     def handle_event(self, event: Event, *args, **kwargs) -> bool:
         if event.type == START_TURN:
-            if self._density < 100:
-                self._density += 1
+            self._stats.grow(event.info)
+        return False
