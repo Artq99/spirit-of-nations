dc32b8f131b8ad62bf97706a95da513482577273
Artq99 <amatracki@gmail.com>
2025-04-19 00:08:27 +0200

    Add basic forest grow mechanic.

--- son/gameplay/map/objects/static/_forest.py
+++ son/gameplay/map/objects/static/_forest.py
@@ -1,8 +1,41 @@
+from typing import List
+
+from pygame import Surface
+from pygame.event import Event
+
+from son.core.events import START_TURN
 from son.core.resources import ResourceManager
-from son.gameplay.map.objects import StaticModifiersHolder
+from son.gameplay.map.objects import MapObject, ModifiersHolder
 
 
-class Forest(StaticModifiersHolder):
+class Forest(MapObject, ModifiersHolder):
+
     def __init__(self, resource_manager: ResourceManager) -> None:
-        super().__init__("Forest", "object.forest", resource_manager)
+        MapObject.__init__(self, "Forest")
+        ModifiersHolder.__init__(self)
+
         self._modifiers.append(("movement_cost", 2))
+
+        self._density = 0
+
+        self._surfaces: List[Surface] = [
+            resource_manager.get_resource("object.forest_01.stage_1"),
+            resource_manager.get_resource("object.forest_01.stage_2"),
+            resource_manager.get_resource("object.forest_01.stage_3"),
+            resource_manager.get_resource("object.forest_01.stage_4")
+        ]
+
+    def _get_surface(self) -> Surface:
+        if self._density < 25:
+            return self._surfaces[0]
+        elif self._density < 50:
+            return self._surfaces[1]
+        elif self._density < 75:
+            return self._surfaces[2]
+        else:
+            return self._surfaces[3]
+
+    def handle_event(self, event: Event, *args, **kwargs) -> bool:
+        if event.type == START_TURN:
+            if self._density < 100:
+                self._density += 1
